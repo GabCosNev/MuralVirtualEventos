cdb7189cdfd8fe77b647468cf90bde6c29e78ccb
Gabriel Costa <nevesgabriel10@gmail.com>
2026-05-22 14:56:21 -0300

    auth em processo

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -6,12 +6,10 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({ adapter });
   }
 
   async onModuleInit() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 }
