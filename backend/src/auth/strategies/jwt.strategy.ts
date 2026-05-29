fe135c8ad0b513563d3de4bf2cd9f9c0d4bf77ca
Gabriel Costa <nevesgabriel10@gmail.com>
2026-05-29 15:36:00 -0300

    Config-Service

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from '../types/jwt-payload.type';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET!,
+      secretOrKey: config.getOrThrow<string>('JWT_SECRET'),
     });
   }
+
   validate(payload: JwtPayload) {
     return { id: payload.sub, email: payload.email, role: payload.role };
   }
