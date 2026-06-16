3584b1319aa82bd42ffe2a36ca8228986f696293
Gabriel Costa <nevesgabriel10@gmail.com>
2026-06-16 14:55:01 -0300

    tela de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthProvider';
 import Layout from './Components/Layout';
 import Home from './pages/Home';
+import Login from './pages/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          <Route path='/login' element={<Login/>}></Route>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
           </Route>
