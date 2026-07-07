5c437f882eb954f9bdbca2ba60d9299ddf7bbcc2
Gabriel Costa <nevesgabriel10@gmail.com>
2026-07-07 15:56:30 -0300

    Alteração nas datas

--- frontend/src/hooks/useCreatePostForm.ts
+++ frontend/src/hooks/useCreatePostForm.ts
@@ -6,8 +6,6 @@ export function useCreatePostForm() {
   const [title, setTitle] = useState("");
   const [eventType, setEventType] = useState<EventType | "">("");
   const [content, setContent] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
   const [startDateInput, setStartDateInput] = useState("");
   const [endDateInput, setEndDateInput] = useState("");
   const [startTimeInput, setStartTimeInput] = useState("");
@@ -25,6 +23,31 @@ export function useCreatePostForm() {
     if (!startDateInput || !endDateInput || !startTimeInput || !endTimeInput)
       return setError("Selecione a data completa do evento");
 
+    const [yearStart, monthStart, dayStart] = startDateInput
+      .split("-")
+      .map(Number);
+    const [hoursStart, minutesStart] = startTimeInput.split(":").map(Number);
+    const dateStartTransform = new Date(
+      yearStart,
+      monthStart - 1,
+      dayStart,
+      hoursStart,
+      minutesStart,
+    );
+
+    const startDate = dateStartTransform.toISOString();
+
+    const [yearEnd, monthEnd, dayEnd] = endDateInput.split("-").map(Number);
+    const [hoursEnd, minutesEnd] = endTimeInput.split(":").map(Number);
+    const dateEndTransform = new Date(
+      yearEnd,
+      monthEnd - 1,
+      dayEnd,
+      hoursEnd,
+      minutesEnd,
+    );
+    const endDate = dateEndTransform.toISOString();
+
     setIsLoading(true);
     try {
       await createPost({
@@ -37,12 +60,10 @@ export function useCreatePostForm() {
       setTitle("");
       setEventType("");
       setContent("");
-      setStartDate("");
       setStartDateInput("");
       setEndDateInput("");
       setStartTimeInput("");
       setEndTimeInput("");
-      setEndDate("");
       setSuccessMessage("Postagem enviada para avaliação!");
     } catch (e: unknown) {
       const err = e as { response?: { data?: { message?: string } } };
@@ -58,10 +79,14 @@ export function useCreatePostForm() {
     setContent,
     eventType,
     setEventType,
-    startDate,
-    setStartDate,
-    endDate,
-    setEndDate,
+    startDateInput,
+    setStartDateInput,
+    endDateInput,
+    setEndDateInput,
+    startTimeInput,
+    setStartTimeInput,
+    endTimeInput,
+    setEndTimeInput,
     error,
     successMessage,
     isLoading,
