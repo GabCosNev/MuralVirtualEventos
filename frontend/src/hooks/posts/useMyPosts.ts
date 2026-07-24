619dc4d275fc6e83ef8676ded11e90b0ff87d57e
Gabriel Costa <nevesgabriel10@gmail.com>
2026-07-24 16:48:35 -0300

    ...

--- frontend/src/hooks/posts/useMyPosts.ts
+++ frontend/src/hooks/posts/useMyPosts.ts
@@ -1,26 +1,6 @@
-import { useState, useEffect } from "react";
+import { usePostsFetch } from "../posts/usePostsFetch";
 import { getPostMine } from "../../services/posts.service";
-import { type Post } from "../../types";
 
-export function useMyPosts() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [isFetching, setIsFetching] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const response = await getPostMine();
-        setPosts(response);
-      } catch (e: unknown) {
-        const err = e as { response?: { data?: { message?: string } } };
-        setError(err.response?.data?.message ?? "Erro ao carregar publicações");
-      } finally {
-        setIsFetching(false);
-      }
-    }
-    fetchPosts();
-  }, []);
-
-  return { posts, isFetching, error };
+export function usePendingPosts() {
+  return usePostsFetch(getPostMine);
 }
