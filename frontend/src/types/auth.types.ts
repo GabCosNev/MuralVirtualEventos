7f015d7ecd46ce5b07f17ab79dccdd58cb199e4a
Gabriel Costa <nevesgabriel10@gmail.com>
2026-08-09 15:24:44 -0300

    ADD novo campo token

--- frontend/src/types/auth.types.ts
+++ frontend/src/types/auth.types.ts
@@ -1,13 +1,15 @@
 export interface Login {
   email: string;
   password: string;
+  turnstileToken: string;
 }
 
 export interface Register {
   name: string;
   email: string;
   password: string;
-  confirmPassword: string
+  confirmPassword: string;
+  turnstileToken: string;
 }
 
 export interface AuthResponse {
